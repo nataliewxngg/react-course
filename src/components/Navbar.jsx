020e8ccbf35aad85c796bc5e73253aecb6fd9591
Natalie Wong <wong.lt.natalie@gmail.com>
2025-08-17 10:30:47 -0400

    Navbar & styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,15 @@
 function Navbar() {
     return (
-        <h1>
-            Navbar goes here
-        </h1>
+        <header className="h-[90px] bg-[#21222a] py-[30px] px-[25px]">
+            <nav className="flex h-full items-center">
+                <img 
+                    src="src/assets/react.svg" 
+                    alt="React Logo"
+                    className="mr-[7px] w-[40px]"
+                />
+                <span className="font-bold text-[#61dafb] text-[1.4rem] ml-[7px]">ReactFacts</span>
+            </nav>
+        </header>
     )
 }
 
