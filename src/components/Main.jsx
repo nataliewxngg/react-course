1fa54c0a9817f136c723f74da770de19b46a2ffc
Natalie Wong <wong.lt.natalie@gmail.com>
2025-08-17 10:46:51 -0400

    Main content section

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,15 @@
 function Main() {
     return (
-        <h1>
-            Main goes here
-        </h1>
+        <main className="px-[25px]">
+            <h1 className="text-[2.4rem] font-bold py-[30px]">Fun facts about React</h1>
+            <ul className="list-disc pl-10 text-1 max-w-[400px]">
+                <li className="py-[10px] leading-[19px]">Was first released in 2013</li>
+                <li className="py-[10px] leading-[19px]">Was originally created by Jordan Walke</li>
+                <li className="py-[10px] leading-[19px]">Has well over 200K stars on GitHub</li>
+                <li className="py-[10px] leading-[19px]">Is maintained by Meta</li>
+                <li className="py-[10px] leading-[19px]">Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
     )
 }
 
