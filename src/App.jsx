b225bf721614360894bf47e2673110ecb5905a2c
Natalie Wong <wong.lt.natalie@gmail.com>
2025-08-18 14:48:54 -0400

    Map entry components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import Header from "./components/Header"
 import Entry from "./components/Entry"
 
+import travelData from "./data/data"
+
 function App() {
+  const travelEntries = travelData.map(location => 
+    <Entry 
+      img={location.img}
+      title={location.title}
+      country={location.country}
+      googleMapsLink={location.googleMapsLink}
+      dates={location.dates}
+      text={location.text}
+    />
+  )
 
   return (
     <>
       <Header />
-      <Entry />
+      <div className="my-3">
+        {travelEntries}
+      </div>
     </>
   )
 }
