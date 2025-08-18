225355e87698a866efef8c33688501ecccd27a56
nataliewxngg <wong.lt.natalie@gmail.com>
2025-08-18 11:13:03 -0400

    Fix image paths

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import Header from "./components/Header"
 import Entry from "./components/Entry"
 
 import Contact from "./components/Contact"
+import mrWhiskerson from "/src/assets/cats/mr-whiskerson.png"
+import fluffyKins from "/src/assets/cats/fluffykins.png"
+import felix from "/src/assets/cats/felix.png"
+import pumpkin from "/src/assets/cats/pumpkin.png"
 
 function App() {
 
@@ -10,25 +14,25 @@ function App() {
       {/* <Header /> */}
       {/* <Entry /> */}
       <Contact 
-        img="/cats/mr-whiskerson.png"
+        img={mrWhiskerson}
         name="Mr. Whiskerson"
         phone="(212) 555-1234"
         email="mr.whiskaz@catnap.meow"
       />
       <Contact 
-        img="/cats/fluffykins.png"
+        img={fluffyKins}
         name="Fluffykins"
         phone="(212) 555-2345"
         email="fluff@me.com"
       />
       <Contact 
-        img="/cats/felix.png"
+        img={felix}
         name="Felix"
         phone="(212) 555-4567"
         email="thecat@hotmail.com"
       />
       <Contact 
-        img="/cats/pumpkin.png"
+        img={pumpkin}
         name="Pumpkin"
         phone="(0800) CAT KING"
         email="pumpkin@scrimba.com"
