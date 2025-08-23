9323313b29c07898babadea23d1e5aa2929e70f5
Natalie Wong <wong.lt.natalie@gmail.com>
2025-08-23 08:13:56 -0400

    Setup project

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,12 @@
-import chefClaudeIcon from "/src/assets/chef-claude-icon.png"
+import trollFace from "../assets/troll-face.png"
 
-function Header() {
+export default function Header() {
     return (
-        <header
-            className="flex justify-center items-center gap-[10px] flex-wrap
-            shadow-[0px_1px_3px_0px_rgba(0,0,0,0.10),0px_1px_2px_0px_rgba(0,0,0,0.06)]
-            py-[16px] px-[8px]">
-            <img src={chefClaudeIcon} alt="Chef Claude Icon" className="w-[30px]"/>
-            <span className="text-[1.3rem]">Chef Claude</span>
+        <header className="header">
+            <img 
+                src={trollFace} 
+            />
+            <h1>Meme Generator</h1>
         </header>
     )
 }
-
-export default Header;
